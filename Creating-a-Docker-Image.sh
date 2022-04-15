49de8c7589835edef2015f11dee97d6f2ee72ff3
seaneon <sean.r.barbour@gmail.com>
2022-04-15 15:47:04 -0400

    forgotten files

--- Creating-a-Docker-Image.sh
+++ Creating-a-Docker-Image.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 cd ~
 sudo usermod -aG docker $USER
-sudo su - $USER
+sleep 5
 mkdir ~/downloads/
+sleep 5
 cd ~/downloads/
 wget https://dl.google.com/go/go1.11.1.linux-amd64.tar.gz
 cd ~
@@ -21,17 +22,11 @@ go mod init
 go get -u github.com/stripe/stripe-go/v72
 go get .
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o webserver .
-wget https://labs.alta3.com/courses/kubernetes/files/scratch -O scratch
-sudo docker build -t webby -f scratch .
+sudo docker build -t webby -f ~/git/k8s-CICD/k8s-docker/scratch .
 sudo docker run -d -p 2345:5000 registry:2
-sudo wget https://labs.alta3.com/courses/kubernetes/bchd.registry_hosts -O /etc/hosts && cat /etc/hosts
-sudo wget https://labs.alta3.com/courses/kubernetes/bchd-reg -O /etc/nginx/sites-enabled/reg
+sudo cp ~/git/k8s-CICD/k8s-config/bchd.registry_hosts /etc/hosts
+sudo cp ~/git/k8s-CICD/bchd-reg /etc/nginx/sites-enabled/reg
 sudo nginx -s reload
 sudo docker tag webby bchd.registry/webby
+sleep 60
 sudo docker push bchd.registry/webby
-
-
-read
-
-
-
