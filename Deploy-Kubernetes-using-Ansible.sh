74f0a3fd1d84e49ff5b3c8d6f0cdea1b3654e09b
seaneon <sean.r.barbour@gmail.com>
2022-04-15 17:26:03 -0400

    mistake with directory structure

--- Deploy-Kubernetes-using-Ansible.sh
+++ Deploy-Kubernetes-using-Ansible.sh
@@ -5,11 +5,11 @@ cd ~/
 fping -A controller-1 controller-2 controller-3 node-1 node-2 node-3
 echo 'source <(kubectl completion bash)' >> ~/.bashrc
 source ~/.bashrc
-cp ~/k8s-CICD/k8s-config/containerd_config.toml ~/containerd_config.toml
-cp ~/k8s-CICD/k8s-config/containerd_update.yaml ~/containerd_update.yaml
-cp ~/k8s-CICD/k8s-config/node-hosts.txt ~/node-hosts.txt
+cp ~/git/k8s-CICD/k8s-config/containerd_config.toml ~/containerd_config.toml
+cp ~/git/k8s-CICD/k8s-config/containerd_update.yaml ~/containerd_update.yaml
+cp ~/git/k8s-CICD/k8s-config/node-hosts.txt ~/node-hosts.txt
 ansible-playbook -i node-hosts.txt containerd_update.yaml
-cp ~/k8s-CICD/k8s-manifests/my_first_pod.yaml ~/my_first_pod.yaml
+cp ~/git/k8s-CICD/k8s-manifests/my_first_pod.yaml ~/my_first_pod.yaml
 kubectl apply -f my_first_pod.yaml
 kubectl get pods
 sleep 30
